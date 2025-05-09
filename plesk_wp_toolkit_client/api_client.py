9bfee45be9e4adc02e0121ee0b563ff5eb56fbc2
kipsang <kipsang1998@gmail.com>
2025-05-09 10:57:38 +0300

    fix: add response codes 3XX as a successful response

--- plesk_wp_toolkit_client/api_client.py
+++ plesk_wp_toolkit_client/api_client.py
@@ -318,7 +318,7 @@ class ApiClient:
                 response_text = response_data.data.decode(encoding)
                 return_data = self.deserialize(response_text, response_type, content_type)
         finally:
-            if not 200 <= response_data.status <= 299:
+            if not 200 <= response_data.status <= 399:
                 raise ApiException.from_response(
                     http_resp=response_data,
                     body=response_text,
